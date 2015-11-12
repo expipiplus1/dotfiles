d0601751d30887d4a32e4cb89319c15ee7dbab37
Joe Hermaszewski <git@monoid.al>
2015-11-12 17:20:07 +0000

    formatting

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -2,25 +2,30 @@
 
 {
   allowUnfree = true;
+  allowBroken = true;
 
   haskellPackageOverrides = with pkgs.haskell.lib; self: super: {
-    ghc-mod = overrideCabal super.ghc-mod (attrs: { 
+    ghc-mod = overrideCabal super.ghc-mod (attrs: {
       src = pkgs.fetchgit{ url = "git://github.com/kazu-yamamoto/ghc-mod.git";
                            rev = "HEAD";
                            sha256 = "0d3s99gggmfpiisfmdr1ka5d97z84hczd0acl5y0gahb5g3r51dx"; }; });
-    hdevtools = overrideCabal super.hdevtools (attrs: { 
+    hdevtools = overrideCabal super.hdevtools (attrs: {
       src = pkgs.fetchgit{ url = "git://github.com/schell/hdevtools.git";
                            rev = "HEAD";
                            sha256 = "06qg2xg40jc77gn7ms8h9xscja3v69paa6yb54iz1pc92l19r180"; }; });
-    hlint   = overrideCabal super.hlint   (attrs: { 
+    hlint   = overrideCabal super.hlint   (attrs: {
       src = pkgs.fetchgit{ url = "git://github.com/ndmitchell/hlint.git";
                            rev = "HEAD";
                            sha256 = "0axi8rkrw10x0a6w5c7cf1l107rp5ckc83v3jbaqyfq8qcap6c3v"; };
       buildDepends = [ super.refact ]; });
-    hindent = overrideCabal super.hindent (attrs: { 
+    hindent = overrideCabal super.hindent (attrs: {
       src = pkgs.fetchgit{ url = "git://github.com/chrisdone/hindent.git";
                            rev = "HEAD";
                            sha256 = "0vmd37j6c4c3jkvnhp6pkw88l3zgasyn3cml54ya6xa8xcgcadi2"; }; });
+    haddock = overrideCabal super.haddock (attrs: {
+      src = pkgs.fetchgit{ url = "git://github.com/haskell/haddock.git";
+                           rev = "ac10a4ccbe416e8612c6ca49b9f19c3a6f4cf25f";
+                           sha256 = "0nmzn6p16xq0kjdpc9y5ahvqcpyqf24626krhfnl85hlcwnq8vvw"; }; });
   };
 
   packageOverrides = super: let pkgs = super.pkgs; in with pkgs; rec {
@@ -29,14 +34,14 @@
     # Irssi with a bunch of perl packages my config needs
     #
     irssi = lib.overrideDerivation super.irssi (oldAttrs: {
-      buildInputs = oldAttrs.buildInputs ++ 
+      buildInputs = oldAttrs.buildInputs ++
                     [ aspell
-                      perlPackages.TextAspell 
+                      perlPackages.TextAspell
                       perlPackages.TextCharWidth
                       perlPackages.CryptX
-                      makeWrapper 
+                      makeWrapper
                     ];
-      postInstall = 
+      postInstall =
         ''
           wrapProgram "$out/bin/irssi" \
             --prefix PERL5LIB : ${perlPackages.TextAspell}/lib/perl5/site_perl/5.20.2/x86_64-linux-thread-multi \
@@ -68,7 +73,7 @@
     #     sha256 = "1sdz8k9nmc904xd0sli2z9cnbqdrk3pg5xnm6d8b24l5k4ljs6n1";
     #     rev = "0bbab3f5b9d43365b2916320aa55d69dc40d6036";
     #     repo = "neovim";
-    #     owner = "expipiplus1"; }; 
+    #     owner = "expipiplus1"; };
     # });
 
 
@@ -90,7 +95,7 @@
     haskell-env = with pkgs; buildEnv {
       name = "haskell-env";
       paths = [
-        (pkgs.haskellPackages.ghcWithHoogle cabalPackages)
+        (pkgs.haskellPackages.ghcWithPackages cabalPackages)
         stack
         cabal-install
         cabal2nix
@@ -128,3 +133,5 @@
     };
   };
 }
+
+
