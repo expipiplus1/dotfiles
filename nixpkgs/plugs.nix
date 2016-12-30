1ac661e85260085cdc6a8c30a77d54c5293d43bd
Joe Hermaszewski <git@monoid.al>
2016-12-30 14:10:13 +0000

    Update vim plugins

--- nixpkgs/plugs.nix
+++ nixpkgs/plugs.nix
@@ -32,12 +32,12 @@ in {
 
 "deoplete.nvim" = {fetchFromGitHub}: vimPlugin rec {
   name = "deoplete.nvim-${version}";
-  version = "2016-11-09";
+  version = "2016-12-25";
   src = fetchFromGitHub {
     owner = "Shougo";
     repo = "deoplete.nvim";
-    rev = "5053fb69bbd26c65dd29e0acd04caf48aa5aa540";
-    sha256 = "1cc0z6bf4d6ipcbkdm91k8fiz3dc1wfbnr30b0yj2n86j5clyljr";
+    rev = "f1e3724bd0b2cbe9dce26261d0939e49d1b96374";
+    sha256 = "1xhn20ybwr45yqnibkc41bpbm87qairib682wgbdr0mwwr3nn8vf";
   };
 };
 
@@ -54,12 +54,12 @@ in {
 
 "haskell-vim" = {stdenv, fetchFromGitHub}: stdenv.mkDerivation (pluginAttrs rec {
   name = "haskell-vim-${version}";
-  version = "2016-11-12";
+  version = "2016-11-30";
   src = fetchFromGitHub {
     owner = "neovimhaskell";
     repo = "haskell-vim";
-    rev = "e6f3093b14f6c603c0e66d8814b9d053843f7133";
-    sha256 = "02k5c4xnlz6xs18inygnh4phayhyx8n0l5haslsy7rp5w7arvmsj";
+    rev = "818d51a9d3d5b37636e2f765551046b90503d29b";
+    sha256 = "1zd3vb3mci9pfqmnrqg3bkl3h0lyv8gnr42jr6fzcc509vfina6r";
   };
   patches = [
     plug-patches/no-space-indent.patch
@@ -91,12 +91,12 @@ in {
 
 "lightline.vim" = {fetchFromGitHub}: vimPlugin rec {
   name = "lightline.vim-${version}";
-  version = "2016-11-06";
+  version = "2016-12-03";
   src = fetchFromGitHub {
     owner = "itchyny";
     repo = "lightline.vim";
-    rev = "836d4c84f76e7066bcfa4c6c60449714cf2b2c45";
-    sha256 = "0qdhpwr4iyifmk0l463s4k5nm8k744ycvxmxgz3s38vkn8q5y06w";
+    rev = "059888ab650fa192dd441e52bd9f41f08b247529";
+    sha256 = "1pa627jjmrhlfbd8yms8lvfgnm0gj9xkr29jkq122icfl6hv3fwx";
   };
 };
 
@@ -146,23 +146,23 @@ in {
 
 "neosnippet.vim" = {fetchFromGitHub}: vimPlugin rec {
   name = "neosnippet.vim-${version}";
-  version = "2016-11-04";
+  version = "2016-12-14";
   src = fetchFromGitHub {
     owner = "Shougo";
     repo = "neosnippet.vim";
-    rev = "22bf9611263fcc9fd2c4bd61031053624ff82714";
-    sha256 = "0f0j0spw8zkvrfhbdlk020y19fh4awhhqwc3ylnxday3q5avxa16";
+    rev = "c179a51f024e27edf4354a52ff8b9fa69c4c803c";
+    sha256 = "1dp96xkbic64k232rhrk44ji149q5hhqhw9ivr89rwqyp9dywkpj";
   };
 };
 
 "neovim-fuzzy" = {fetchFromGitHub}: vimPlugin rec {
   name = "neovim-fuzzy-${version}";
-  version = "2016-10-26";
+  version = "2016-12-14";
   src = fetchFromGitHub {
     owner = "cloudhead";
     repo = "neovim-fuzzy";
-    rev = "aa6681f2b8b2f2de2c851d0ecff6b548f94e51d6";
-    sha256 = "1fimpfc8x3dawgfj3k3mbj9sf9s2x3m66hz62b811d74wdix7az2";
+    rev = "8832151646d08d69c3cf330ebb8f365764e62245";
+    sha256 = "06rsp6ryq6y7dlladiipgrial42rlr0rkqk6svx6v952l80x0gsb";
   };
 };
 
@@ -256,12 +256,12 @@ in {
 
 "vim-fugitive" = {fetchFromGitHub}: vimPlugin rec {
   name = "vim-fugitive-${version}";
-  version = "2016-11-05";
+  version = "2016-11-13";
   src = fetchFromGitHub {
     owner = "tpope";
     repo = "vim-fugitive";
-    rev = "b3a8be6975dcc27b523b030bf5d35d9c737aef18";
-    sha256 = "1qizj0xmpaj32b9f8p2d76s5rsvm8cxv0rh5yiczlgfvflbi587r";
+    rev = "b754bc2031f21a532c083dd0d072ba373bbe3a37";
+    sha256 = "1sig8dl3m1dw5zjxdsp00n1cacmcwdvas3iz04zk88v6xsm8rj22";
   };
 };
 
@@ -289,12 +289,12 @@ in {
 
 "vim-hoogle" = {fetchFromGitHub}: vimPlugin rec {
   name = "vim-hoogle-${version}";
-  version = "2015-12-24";
+  version = "2016-11-29";
   src = fetchFromGitHub {
     owner = "Twinside";
     repo = "vim-hoogle";
-    rev = "a5db36f048ac16ab9774fc86f36cd4ae9a444932";
-    sha256 = "1855n6dsimbc0brfbmmkx1gszhmgfghm1h1mpi95mq9qzbwkgjri";
+    rev = "9f00214ece60b9514ef2911fb62e7d53c52d3b4c";
+    sha256 = "1yh732s9k3lhsdqz8qfqijvl0za7cl9mndbzqh4dg2g14f5f5qqw";
   };
 };
 
@@ -355,12 +355,12 @@ in {
 
 "vim-startify" = {fetchFromGitHub}: vimPlugin rec {
   name = "vim-startify-${version}";
-  version = "2016-10-31";
+  version = "2016-12-26";
   src = fetchFromGitHub {
     owner = "mhinz";
     repo = "vim-startify";
-    rev = "9732427469c6933cbedeb7e662c70a2aaf4e63d2";
-    sha256 = "022ibyqq140130x8c2901sskh03q516ghnc25i41qv6b2hcsq1ad";
+    rev = "72f398ebe20ff91a8105222d64f8ef0089341688";
+    sha256 = "05imqwvbl6d98xss4csvvlyig1rxzqxa3jq7yvlz6g9cq1lw3lc2";
   };
 };
 
