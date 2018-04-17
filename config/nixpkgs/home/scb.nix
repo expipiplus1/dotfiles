445b2c6d2aab80bba6da3ff8854b9d4d96f55d19
Joe Hermaszewski <git@monoid.al>
2018-04-17 15:41:18 +0800

    scb Make tmux not override DISPLAY or XAUTHORITY

--- config/nixpkgs/home/scb.nix
+++ config/nixpkgs/home/scb.nix
@@ -11,4 +11,6 @@
     http = { sslcainfo = config.home.sessionVariables.SSL_CERT_FILE; };
     hub = { protocol = "https"; };
   };
+
+  programs.tmux.update-environment = [ "SSH_ASKPASS" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "SSH_CONNECTION" "WINDOWID" "DBUS_SESSION_BUS_ADDRESS" ];
 }
