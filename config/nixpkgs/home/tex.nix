1a161571e93a7ecda9a4e6ee0a26108a4c4d3274
Joe Hermaszewski <git@monoid.al>
2018-06-04 10:58:59 +0800

    scb: update pandocenv

--- config/nixpkgs/home/tex.nix
+++ config/nixpkgs/home/tex.nix
@@ -9,8 +9,8 @@
         collection-fontsrecommended draftwatermark ec eso-pic etoolbox euenc
         everypage filehook fontspec greek-inputenc koma-script lm mathspec
         mdframed metafont microtype needspace parskip pgf pgfgantt pgfkeyx
-        scheme-basic siunitx standalone ucharcat unicode-math wallpaper xcolor
-        xetex xkeyval xunicode zapfding;
+        scheme-basic siunitx standalone ucharcat unicode-math vntex wallpaper
+        xcolor xetex xkeyval xunicode zapfding;
     };
   };
 }
