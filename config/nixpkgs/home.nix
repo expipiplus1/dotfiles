fc14c02f383335bedb2e483f00fcd2e13a5652b8
Joe Hermaszewski <git@monoid.al>
2019-10-12 15:20:08 +0800

    nixfmt

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -12,16 +12,12 @@ let
 
     in pkgs.linkFarm "zsh-custom" links;
 
-in
-
-{
+in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   news.display = "silent";
 
-  home.packages = with pkgs; [
-    gitAndTools.hub
-  ];
+  home.packages = with pkgs; [ gitAndTools.hub ];
 
   programs.zsh = {
     enable = true;
@@ -41,27 +37,21 @@ in
         };
       });
       theme = "spaceship";
-      plugins = [
-        "cabal"
-        "gitfast"
-        "history-substring-search"
-        "nix"
-        "tmux"
-        "vi-mode"
-      ];
+      plugins =
+        [ "cabal" "gitfast" "history-substring-search" "nix" "tmux" "vi-mode" ];
     };
     shellAliases = {
-      git="${pkgs.gitAndTools.hub}/bin/hub";
-      gs="git status";
-      gd="git diff";
-      cb="cabal build -j8";
-      nb="nix-build -j8";
-      ne="nix-env -f '<nixpkgs>'";
+      git = "${pkgs.gitAndTools.hub}/bin/hub";
+      gs = "git status";
+      gd = "git diff";
+      cb = "cabal build -j8";
+      nb = "nix-build -j8";
+      ne = "nix-env -f '<nixpkgs>'";
     };
     localVariables = {
-      HYPHEN_INSENSITIVE="true";
-      DISABLE_AUTO_UPDATE="true";
-      ENABLE_CORRECTION="true";
+      HYPHEN_INSENSITIVE = "true";
+      DISABLE_AUTO_UPDATE = "true";
+      ENABLE_CORRECTION = "true";
     };
     initExtraBeforeCompInit = ''
       export NIX_PATH=''${NIX_PATH:+$NIX_PATH:}home-manager=\$HOME/src/home-manager
@@ -152,28 +142,31 @@ in
     userEmail = "git@monoid.al";
     userName = "Joe Hermaszewski";
     aliases = {
-      po      = ''"!git push --set-upstream origin \"$(git rev-parse --abbrev-ref HEAD)\""'';
-      s       = ''"status -s"'';
-      cp      = ''"cherry-pick"'';
-      co      = ''"checkout"'';
-      cob     = ''"checkout -b"'';
-      pr      = ''"pull-request"'';
-      latest  = ''"!git log --all --oneline | head -n1 | cut -f1 -d' '"'';
-      cpl     = ''"!git cherry-pick $(git latest)"'';
-      pf      = ''"push --force-with-lease"'';
-      authors = ''"!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -n ; }; f"'';
-      author  = ''"!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -nr | head -n1 | sed 's/ *[0-9]* *//' ; }; f"'';
-      cane    = ''"commit --amend --no-edit"'';
+      po = ''
+        "!git push --set-upstream origin \"$(git rev-parse --abbrev-ref HEAD)\""'';
+      s = ''"status -s"'';
+      cp = ''"cherry-pick"'';
+      co = ''"checkout"'';
+      cob = ''"checkout -b"'';
+      pr = ''"pull-request"'';
+      latest = ''"!git log --all --oneline | head -n1 | cut -f1 -d' '"'';
+      cpl = ''"!git cherry-pick $(git latest)"'';
+      pf = ''"push --force-with-lease"'';
+      authors = ''
+        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -n ; }; f"'';
+      author = ''
+        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -nr | head -n1 | sed 's/ *[0-9]* *//' ; }; f"'';
+      cane = ''"commit --amend --no-edit"'';
     };
     extraConfig = {
       oh-my-zsh = { only-branch = 1; };
-      rebase    = { instructionFormat = "[%an] %s"; };
-      github    = { user = "expipiplus1"; };
+      rebase = { instructionFormat = "[%an] %s"; };
+      github = { user = "expipiplus1"; };
       mergetool = { keepBackup = false; };
-      merge     = { tool = "vimdiff"; };
-      core      = { editor = "vim"; };
-      push      = { default = "simple"; };
-      color     = { ui = "auto"; };
+      merge = { tool = "vimdiff"; };
+      core = { editor = "vim"; };
+      push = { default = "simple"; };
+      color = { ui = "auto"; };
     };
   };
 }
