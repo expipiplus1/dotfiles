080ea30eebabc798d46c054567e6d095dec38774
Joe Hermaszewski <git@monoid.al>
2019-10-12 21:45:55 +0800

    neaten

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./home/zsh.nix ./home/git.nix ./home/tmux.nix ./home/neovim.nix ./home/tex.nix ];
+  imports = [
+    ./home/zsh.nix
+    ./home/git.nix
+    ./home/tmux.nix
+    ./home/neovim.nix
+    ./home/tex.nix
+  ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
