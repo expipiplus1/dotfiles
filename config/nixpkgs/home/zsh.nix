7a7a1b43c52922a6d283553a4e6e512983fbd113
Joe Hermaszewski <git@monoid.al>
2019-10-14 11:25:58 +0800

    Add highlight function

--- config/nixpkgs/home/zsh.nix
+++ config/nixpkgs/home/zsh.nix
@@ -124,6 +124,25 @@ in {
         nix-store -q --graph "$1" | dijkstra -da "$2" | gvpr -c 'N[dist>1000.0]{delete(NULL, $)}' | dot -Tsvg | display
       }
 
+      function highlight() {
+        declare -A fg_color_map
+        fg_color_map[black]=30
+        fg_color_map[red]=31
+        fg_color_map[green]=32
+        fg_color_map[yellow]=33
+        fg_color_map[blue]=34
+        fg_color_map[magenta]=35
+        fg_color_map[cyan]=36
+
+        fg_c=$(echo -e "\e[1;''${fg_color_map[$1]}m")
+        c_rs=$'\e[0m'
+        sed -u s"/$2/$fg_c\0$c_rs/gI"
+      }
+
+      function color-results () {
+        highlight red 'Failure\|Error' | highlight green 'Success' | highlight yellow 'Warning'
+      }
+
       unsetopt AUTO_CD
 
       unsetopt share_history
