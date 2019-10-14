474d46bebf828e70c4d0a6e9e46ac43000478aa6
Joe Hermaszewski <git@monoid.al>
2019-10-14 20:49:19 +0800

    Remove pc only vim plugins

--- config/nixpkgs/home/neovim.nix
+++ config/nixpkgs/home/neovim.nix
@@ -103,14 +103,6 @@ let
     tmux-complete-vim
     gist-vim
     (appendPatches [ ./plug-patches/cabal-module-word.patch ] haskell-vim)
-    {
-      plugin = hlint-refactor-vim;
-      config = ''
-        let g:hlintRefactor#disableDefaultKeybindings = 1
-        map <silent> <nowait> <leader>e :call ApplyOneSuggestion()<CR>
-        map <silent> <nowait> <leader>E :call ApplyAllSuggestions()<CR>
-      '';
-    }
     lessspace-vim
     {
       plugin = lightline-vim;
@@ -190,8 +182,6 @@ let
         nnoremap <C-p> :FuzzyOpen<CR>
       '';
     }
-    open-browser-vim
-    open-browser-github-vim
     {
       plugin = prev_indent;
       config = ''
@@ -250,42 +240,6 @@ let
     vim-unimpaired
     vim-visual-increment
     vim-yaml
-    {
-      plugin = LanguageClient-neovim;
-      config = ''
-        let g:LanguageClient_serverCommands = {
-          \ 'haskell': ['hie', '--lsp'],
-          \ }
-
-        map <Leader>ll :call LanguageClient_contextMenu()<CR>
-        map <Leader>lk :call LanguageClient#textDocument_hover()<CR>
-        map <Leader>lg :call LanguageClient#textDocument_definition()<CR>
-        map <Leader>lr :call LanguageClient#textDocument_rename()<CR>
-        map <Leader>lf :call LanguageClient#textDocument_rangeFormatting()<CR>
-        map <Leader>ld :call LanguageClient#textDocument_formatting()<CR>
-        map <Leader>lb :call LanguageClient#textDocument_references()<CR>
-        map <Leader>la :call LanguageClient#textDocument_codeAction()<CR>
-        map <Leader>ls :call LanguageClient#textDocument_documentSymbol()<CR>
-        map <Leader>lh :call LanguageClient#textDocument_documentHighlight()<CR>
-        map <Leader>le :call LanguageClient#workspace_applyEdit()<CR>
-        nnoremap <nowait> <leader>R :call LanguageClient#textDocument_rename()<CR>
-
-        " Rename - rn => rename
-        noremap <leader>rn :call LanguageClient#textDocument_rename()<CR>
-
-        " Rename - rc => rename camelCase
-        noremap <leader>rc :call LanguageClient#textDocument_rename(
-                    \ {'newName': Abolish.camelcase(expand('<cword>'))})<CR>
-
-        " Rename - rs => rename snake_case
-        noremap <leader>rs :call LanguageClient#textDocument_rename(
-                    \ {'newName': Abolish.snakecase(expand('<cword>'))})<CR>
-
-        " Rename - ru => rename UPPERCASE
-        noremap <leader>ru :call LanguageClient#textDocument_rename(
-                    \ {'newName': Abolish.uppercase(expand('<cword>'))})<CR>
-      '';
-    }
   ];
 
   pluginConfig = p:
