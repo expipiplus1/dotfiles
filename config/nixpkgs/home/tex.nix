66d7be951308e075faa6581679b1a1a6005c6aa5
Joe Hermaszewski <git@monoid.al>
2019-10-14 11:24:08 +0800

    scb: Add fancyvrb tex package

--- config/nixpkgs/home/tex.nix
+++ config/nixpkgs/home/tex.nix
@@ -7,7 +7,7 @@
       inherit (tpkgs)
         amsmath babel beamer booktabs cm-super collection-fontsextra
         collection-fontsrecommended draftwatermark ec eso-pic etoolbox euenc
-        everypage filehook fontspec greek-inputenc koma-script lm mathspec
+        everypage fancyvrb filehook fontspec greek-inputenc koma-script lm mathspec
         mdframed metafont microtype needspace parskip pgf pgfgantt pgfkeyx
         scheme-basic siunitx standalone ucharcat unicode-math vntex wallpaper
         xcolor xetex xkeyval xunicode zapfding;
