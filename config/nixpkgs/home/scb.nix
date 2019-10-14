9663f603f7cdb85901f00d67241b142b9267e73c
Joe Hermaszewski <git@monoid.al>
2019-10-14 11:25:16 +0800

    scb: Add JIRA functions

--- config/nixpkgs/home/scb.nix
+++ config/nixpkgs/home/scb.nix
@@ -15,18 +15,35 @@
 
   programs.tmux.update-environment = [ "SSH_ASKPASS" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "SSH_CONNECTION" "WINDOWID" "DBUS_SESSION_BUS_ADDRESS" ];
 
-  programs.zsh.initExtra = ''
-    zle-keymap-select () {
-      if [ $KEYMAP = vicmd ]; then
-        # the command mode for vi
-        echo -ne "\e[2 q"
-      else
-        # the insert mode for vi
-        echo -ne "\e[5 q"
-      fi
-    }
+  programs.zsh = {
+    initExtraBeforeCompInit = ''
+      j(){
+        ${pkgs.atlassian-jira}/bin/jira --endpoint=https://jira.global.standardchartered.com --user="$STAFFID" "$@"
+      }
 
-    # Start with insert mode cursor
-    echo -ne "\e[5 q"
-  '';
+      jc(){
+        j create --project="$1" --issuetype=Task --override summary="$2" --override description="$3" --noedit
+      }
+
+      # Assigns to me and makes in progress
+      jira-start(){
+        j edit "$1" --noedit --override=assignee="'"$bankid"'" --override=reporter="'"$bankid"'"
+        j transition "In Progress" "$1" --noedit
+      }
+    '';
+    initExtra = ''
+      zle-keymap-select () {
+        if [ $KEYMAP = vicmd ]; then
+          # the command mode for vi
+          echo -ne "\e[2 q"
+        else
+          # the insert mode for vi
+          echo -ne "\e[5 q"
+        fi
+      }
+
+      # Start with insert mode cursor
+      echo -ne "\e[5 q"
+    '';
+  };
 }
