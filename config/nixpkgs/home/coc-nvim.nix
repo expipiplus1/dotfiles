e1312ccd7a22b6fbb0fc2bf8f7c0765906f8ae6b
Joe Hermaszewski <git@monoid.al>
2019-10-19 09:26:33 +0800

    Improve coc config

--- config/nixpkgs/home/coc-nvim.nix
+++ config/nixpkgs/home/coc-nvim.nix
@@ -25,6 +25,8 @@
       nmap <silent> gi <Plug>(coc-implementation)
       nmap <silent> gr <Plug>(coc-references)
 
+      nmap <leader>e  <Plug>(coc-references)
+
       "
       " Coc visuals
       "
@@ -42,6 +44,25 @@
       nmap <leader>ca  <Plug>(coc-codeaction)
       " Fix autofix problem of current line
       nmap <leader>qf  <Plug>(coc-fix-current)
+
+
+      "
+      " Coc completion
+      "
+
+      " use <tab> for trigger completion and navigate to the next complete item
+      function! s:check_back_space() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~ '\s'
+      endfunction
+
+      inoremap <silent><expr> <Tab>
+            \ pumvisible() ? "\<C-n>" :
+            \ <SID>check_back_space() ? "\<Tab>" :
+            \ coc#refresh()
+
+      inoremap <expr> <Tab> pumvisible() ? "\<C-n>" : "\<Tab>"
+      inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
     '';
   };
 
@@ -56,7 +77,7 @@
           requireRootPattern = true;
           filetypes = [ "hs" "lhs" "haskell" ];
           initializationOptions = {
-            languageServerHaskell = { hlintOn = true; };
+            languageServerHaskell = { };
           };
         };
       };
