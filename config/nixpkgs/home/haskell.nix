c8cfb9894eb58cb18f859d9e3c2597741af1c1d2
Joe Hermaszewski <git@monoid.al>
2020-02-23 21:46:55 +0800

    Use our nixpkgs in hie

--- config/nixpkgs/home/haskell.nix
+++ config/nixpkgs/home/haskell.nix
@@ -89,7 +89,7 @@ in {
         repo = "all-hies";
         rev = "92148680060ed68f24738128d8489f4f9387d2ff";
         sha256 = "1yb75f8p09dp4yx5d3w3wvdiflaav9a5202xz9whigk2p9ndwbp5";
-      }) { }).unstableFallback.selection {
+      }) { inherit pkgs; }).unstableFallback.selection {
         selector = p: { inherit (p) ghc865 ghc882; };
       })
     ];
