fe16cf2c8433abd321c0d6963550bb825cae9260
Joe Hermaszewski <git@monoid.al>
2020-02-27 20:14:39 +0800

    Make install.sh useful again
    TODO: neaten it up

--- install.sh
+++ install.sh
@@ -1,55 +1,24 @@
 #!/usr/bin/env bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#!nix-shell -i bash -p git
 
-shopt -s extglob
+cd "$HOME" || exit
+mkdir -p src
+cd src || exit
 
-########## Variables
+git clone https://github.com/nixos/nixpkgs
+cd nixpkgs || exit
+git remote add channels https://github.com/nixos/nixpkgs-channels
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="agignore
-       bashrc
-       config/nixpkgs/home.nix
-       ghci
-       aspell.conf
-       xinitrc
-       xkb/symbols/local
-       xkb/types/local
-       xkb/keymap/custom
-       XCompose
-       Xmodmap
-       haskeline
-       brittany
-       stylish-haskell
-      "
+cd "$HOME/src" || exit
+git clone https://github.com/rycee/home-manager
+cd home-manager || exit
+git remote add expipiplus1 https://github.com/expipiplus1/home-manager
+git fetch expipiplus1
+git checkout --track expipiplus1/joe
 
-##########
+nix-shell . -A install
 
-echo "updating submodules"
-git submodule update --init
+rm "$HOME/.config/nixpkgs/home.nix"
+ln -s "$HOME/dotfiles/config/nixpkgs/home.nix" "$HOME/.config/nixpkgs/home.nix"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-  if [[ -e ~/.$file ]]; then
-    mkdir -p $olddir
-    if [[ -e $olddir/.$file ]]; then
-      echo "Removing $olddir/.$file"
-      rm -r $olddir/.$file
-    fi
-    echo "Moving existing ~/.$file from ~ to $olddir/.$file"
-    mkdir -p $(dirname $olddir/.$file)
-    mv ~/.$file $olddir/.$file
-  fi
-  mkdir -p $(dirname ~/.$file)
-  ln -sv $dir/$file ~/.$file
-done
-
-gsettings set org.gnome.desktop.media-handling automount-open false
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+NIX_PATH="nixpkgs=$HOME/src/nixpkgs:home-manager=$HOME/src/home-manager" home-manager switch
