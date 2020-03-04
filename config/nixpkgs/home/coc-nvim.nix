49ff9142ce2edc08efef641728adb3e604280379
Joe Hermaszewski <git@monoid.al>
2020-03-04 16:01:31 +0800

    Force hoogle for hie

--- config/nixpkgs/home/coc-nvim.nix
+++ config/nixpkgs/home/coc-nvim.nix
@@ -127,7 +127,7 @@
         trace.server = "verbose";
         hieExecutablePath = pkgs.writeShellScript "nix-shell-hie" ''
           if [[ -f default.nix || -f shell.nix ]]; then
-            ${pkgs.cached-nix-shell}/bin/cached-nix-shell --run "hie -l /tmp/hie.log $(printf "''${1+ %q}" "$@")"
+            ${pkgs.cached-nix-shell}/bin/cached-nix-shell --arg hoogle true --run "hie -l /tmp/hie.log $(printf "''${1+ %q}" "$@")"
           else
             exec hie "$@"
           fi
