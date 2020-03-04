b4a5dd6aaaaab819847e2772121f836609932f11
Joe Hermaszewski <git@monoid.al>
2020-03-04 14:20:29 +0800

    Correct env var and proxy

--- config/nixpkgs/home/scb.nix
+++ config/nixpkgs/home/scb.nix
@@ -1,17 +1,21 @@
 { config, pkgs, lib, ... }:
 
-{
+rec {
   home.sessionVariables = rec {
     http_proxy = "SG-MBFC-LAN-PRX01.global.standardchartered.com:8080";
     https_proxy = http_proxy;
     SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-    CORTEX_EFFECTIVE_USER_ID = builtins.getEnv "STAFFID";
+    CORTEX_EFFECTIVE_USER_ID = builtins.getEnv "BANKID";
   };
 
   home.packages = [ pkgs.weechat ];
 
-  programs.git.extraConfig = {
-    http = { sslcainfo = config.home.sessionVariables.SSL_CERT_FILE; };
+  programs.git.extraConfig = rec {
+    http = {
+      sslcainfo = config.home.sessionVariables.SSL_CERT_FILE;
+      proxy = home.sessionVariables.http_proxy;
+    };
+    https = http;
     hub = { protocol = "https"; };
   };
 
