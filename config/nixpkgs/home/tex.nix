ec83e506bf459c56251bf1b8ff0b4a5efbbe112c
Joe Hermaszewski <git@monoid.al>
2020-04-27 20:49:15 +0800

    More tex packages

--- config/nixpkgs/home/tex.nix
+++ config/nixpkgs/home/tex.nix
@@ -1,21 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    pandoc
-    pdftk
-  ];
+  home.packages = with pkgs; [ pandoc pdftk ];
 
   programs.texlive = {
     enable = true;
-    extraPackages = tpkgs: {
-      inherit (tpkgs)
-        amsmath babel beamer booktabs cm-super collection-fontsextra
-        collection-fontsrecommended draftwatermark ec eso-pic etoolbox euenc
-        everypage fancyvrb filehook fontspec greek-inputenc koma-script lm mathspec
-        mdframed metafont microtype needspace parskip pgf pgfgantt pgfkeyx
-        scheme-basic siunitx standalone ucharcat unicode-math vntex wallpaper
-        xcolor xetex xkeyval xunicode zapfding;
-    };
+    extraPackages = tpkgs:
+      {
+        inherit (tpkgs)
+          amsmath babel beamer booktabs cm-super collection-fontsextra
+          collection-fontsrecommended draftwatermark ec eso-pic etoolbox euenc
+          everypage fancyvrb filehook fontspec greek-inputenc koma-script lm
+          mathspec mdframed metafont microtype needspace parskip pgf pgfgantt
+          pgfkeyx scheme-basic siunitx standalone ucharcat unicode-math vntex
+          wallpaper xcolor xetex xkeyval xunicode zapfding;
+      } // { # Doxygen
+        inherit (tpkgs)
+          float varwidth multirow hanging adjustbox stackengine ulem sectsty
+          tocloft refman newunicodechar caption etoc collectbox listofitems;
+      };
   };
 }
