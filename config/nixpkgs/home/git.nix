d8c9d0d09a6b387a3688ddac04bf1f2ac3129eea
Joe Hermaszewski <git@monoid.al>
2020-09-23 23:20:22 +0800

    Remove unnecessary quotes for git aliases

--- config/nixpkgs/home/git.nix
+++ config/nixpkgs/home/git.nix
@@ -18,20 +18,20 @@
     userName = "Joe Hermaszewski";
     aliases = {
       po = ''
-        "!git push --set-upstream origin \"$(git rev-parse --abbrev-ref HEAD)\""'';
-      s = ''"status -s"'';
-      cp = ''"cherry-pick"'';
-      co = ''"checkout"'';
-      cob = ''"checkout -b"'';
-      pr = ''"pull-request"'';
-      latest = ''"!git log --all --oneline | head -n1 | cut -f1 -d' '"'';
-      cpl = ''"!git cherry-pick $(git latest)"'';
-      pf = ''"push --force-with-lease"'';
-      authors = ''
-        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -n ; }; f"'';
-      author = ''
-        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -nr | head -n1 | sed 's/ *[0-9]* *//' ; }; f"'';
-      cane = ''"commit --amend --no-edit"'';
+        !git push --set-upstream origin \"$(git rev-parse --abbrev-ref HEAD)\"'';
+      s = "status -s";
+      cp = "cherry-pick";
+      co = "checkout";
+      cob = "checkout -b";
+      pr = "pull-request";
+      latest = "!git log --all --oneline | head -n1 | cut -f1 -d' '";
+      cpl = "!git cherry-pick $(git latest)";
+      pf = "push --force-with-lease";
+      authors =
+        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -n ; }; f";
+      author =
+        "!f(){ set -o pipefail; git blame $1 --line-porcelain | grep 'author ' | grep -v 'Not Committed Yet' | sed 's/author //' | sort | uniq -c | sort -nr | head -n1 | sed 's/ *[0-9]* *//' ; }; f";
+      cane = "commit --amend --no-edit";
     };
     extraConfig = rec {
       oh-my-zsh = { only-branch = 1; };
