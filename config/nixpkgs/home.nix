7352e9ac61c429ad225e38f4b355a8209f55e4f0
Joe Hermaszewski <git@monoid.al>
2020-11-28 12:48:34 +0800

    make home-manager a submodule

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -38,13 +38,13 @@ in {
   # Let Home Manager install and manage itself.
   programs.home-manager = {
     enable = true;
-    path = builtins.getEnv "HOME" + "/src/home-manager";
+    path = toString ../../home-manager;
   };
   news.display = "silent";
 
   home.sessionVariables = {
     LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-    NIX_PATH = "nixpkgs=$HOME/src/nixpkgs:home-manager=$HOME/src/home-manager";
+    NIX_PATH = "nixpkgs=$HOME/src/nixpkgs:home-manager=${toString ../../home-manager}";
     EDITOR = "vim";
   };
 
