b5e7f83e4b24e1f8d46af774db19be989167de05
Ellie Hermaszewska <git@monoid.al>
2022-07-28 16:45:37 +0800

    nvim dap settings

--- config/nixpkgs/home/neovim.nix
+++ config/nixpkgs/home/neovim.nix
@@ -289,6 +289,52 @@ in {
           autocmd FileType c setlocal comments=://
         '';
       }
+      {
+        plugin = nvim-dap;
+        config = ''
+          nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
+          nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
+          nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
+          nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
+          nnoremap <silent> <leader>b :lua require'dap'.toggle_breakpoint()<CR>
+          nnoremap <silent> <leader>B :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+          nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+          " nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
+          " nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
+        '' + luaConfig ''
+          local dap = require('dap')
+          dap.adapters.cppdbg = {
+            id = 'cppdbg',
+            type = 'executable',
+            command = '${(x: y: x) "" pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7',
+          }
+          dap.configurations.cpp = {
+            {
+              name = "Launch file",
+              type = "cppdbg",
+              request = "launch",
+              program = function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+              end,
+              cwd = ''\'''${workspaceFolder}',
+              stopOnEntry = true,
+            },
+            {
+              name = 'Attach to gdbserver :1234',
+              type = 'cppdbg',
+              request = 'launch',
+              MIMode = 'gdb',
+              miDebuggerServerAddress = 'localhost:1234',
+              miDebuggerPath = '/usr/bin/gdb',
+              cwd = ''\'''${workspaceFolder}',
+              program = function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+              end,
+            },
+          }
+        '';
+      }
+      nvim-dap-ui
     ];
     initExtra = ''
       source ${
