2f7e943c9845b0a2f30d23b713b1d3115cf4cec1
Ellie Hermaszewska <git@monoid.al>
2023-07-28 16:12:45 +0800

    Correct comment string

--- config/nixpkgs/home/neovim.nix
+++ config/nixpkgs/home/neovim.nix
@@ -36,13 +36,6 @@ in {
       (appendPatches [ ./plug-patches/cabal-module-word.patch ] haskell-vim)
       vim-strip-trailing-whitespace
       {
-        plugin = mini-nvim;
-        type = "lua";
-        config = ''
-          require('mini.comment').setup()
-        '';
-      }
-      {
         plugin = treesj;
         type = "lua";
         config = ''
@@ -51,27 +44,6 @@ in {
           })
           -- p for parameter
           vim.keymap.set('n', '<leader>p', require('treesj').toggle)
-        '';
-      }
-      {
-        plugin = nvim-ts-context-commentstring;
-        type = "lua";
-        config = ''
-          require'nvim-treesitter.configs'.setup {
-            context_commentstring = {
-              enable = true,
-              config = {
-                css = '// %s',
-                c = '// %s',
-                cpp = '// %s',
-                vhdl = '-- %s',
-                haskell = '-- %s',
-                json = '// %s',
-                hlsl = '// %s',
-                slang = '// %s',
-              },
-            }
-          }
         '';
       }
       nvim-treesitter-textobjects
@@ -112,6 +84,55 @@ in {
           }
         '' + ''
           hi! def link TreesitterContext StatusLine
+        '';
+      }
+      {
+        plugin = nvim-ts-context-commentstring;
+        type = "lua";
+        config = ''
+          require'nvim-treesitter.configs'.setup {
+            context_commentstring = {
+              enable = true,
+              config = {
+                css = '// %s',
+                c = '// %s',
+                cpp = '// %s',
+                vhdl = '-- %s',
+                haskell = '-- %s',
+                json = '// %s',
+                hlsl = '// %s',
+                slang = '// %s',
+              },
+            }
+          }
+        '';
+      }
+      {
+        plugin = mini-nvim;
+        type = "lua";
+        config = ''
+          -- require('mini.pairs').setup({
+          --   mappings = {
+          --     ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\][\n ]' },
+          --     ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\][\n ]' },
+          --     ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\][\n ]' },
+          --
+          --     [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\][\n ]' },
+          --     [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\][\n ]' },
+          --     ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\][\n ]' },
+          --
+          --     ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\][\n ]', register = { cr = false } },
+          --     ["'"] = { action = 'closeopen', pair = "'''", neigh_pattern = '[^%a\\][\n ]', register = { cr = false } },
+          --     ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\][\n ]', register = { cr = false } },
+          --   },
+          -- })
+          require('mini.comment').setup({
+            options = {
+              custom_commentstring = function()
+                return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
+              end,
+            },
+          })
         '';
       }
       {
