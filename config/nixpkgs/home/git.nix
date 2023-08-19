ff17daa4733fb254d55b85bdc8b6009a748f5dd4
Ellie Hermaszewska <git@monoid.al>
2023-08-19 10:48:14 +0800

    add gitui

--- config/nixpkgs/home/git.nix
+++ config/nixpkgs/home/git.nix
@@ -101,4 +101,9 @@
       color = { ui = "auto"; };
     };
   };
+
+  programs.gitui = {
+    enable = true;
+    keyConfig = builtins.readFile (config.programs.gitui.package.src + "/vim_style_key_config.ron");
+  };
 }
