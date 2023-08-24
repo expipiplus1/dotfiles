59012a055c5b298481c1a40ff1348aaacd25c353
Ellie Hermaszewska <git@monoid.al>
2023-08-24 12:04:31 +0800

    home-manager flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,30 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fu.url = "github:numtide/flake-utils";
+    hm = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, fu, hm }@inputs: {
     nixosConfigurations.sophie = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      specialArgs = inputs;
       modules = [ (import ./hosts/sophie) ];
+    };
+
+    homeConfigurations = {
+      "e@sophie" = hm.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (nixpkgs.lib.getName pkg)
+            [ "vscode-extension-ms-vscode-cpptools" ];
+        };
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./config/nixpkgs/home.nix ];
+      };
     };
   };
 }
