768995b168b314af7d705019a6d4106450260633
Ellie Hermaszewska <git@monoid.al>
2023-08-24 13:48:58 +0800

    quiet direnv

--- home/direnv.nix
+++ home/direnv.nix
@@ -5,4 +5,12 @@
     enable = true;
     nix-direnv.enable = true;
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      direnv = super.direnv.overrideAttrs (old: {
+        patches = old.patches or [ ] ++ [ ../patches/quiet-direnv.patch ];
+      });
+    })
+  ];
 }
