b9bfb01040e20c9eedcd0e6502de5f74f3e238b5
Ellie Hermaszewska <git@monoid.al>
2023-09-25 11:46:29 +0800

    add kde

---
+++ hosts/shared/kde.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+    displayManager.defaultSession = "plasmawayland";
+  };
+
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
+
+  environment.systemPackages = with pkgs; [ kdeplasma-addons ];
+}
+
