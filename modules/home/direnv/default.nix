a99a063de9aca63ec0ada71f809bc32a51b60100
Ellie Hermaszewska <git@monoid.al>
2024-09-05 13:17:38 +0800

    silent direnv

--- modules/home/direnv/default.nix
+++ modules/home/direnv/default.nix
@@ -3,9 +3,8 @@ lib.internal.simpleModule inputs "direnv" {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-  };
 
-  programs.zsh.initExtra = ''
-    export DIRENV_LOG_FORMAT=$(printf "\e[1;30""mdirenv: %%s\e[0")
-  '';
+    # silent  = true;
+    config = { global.hide_env_diff = true; };
+  };
 }
