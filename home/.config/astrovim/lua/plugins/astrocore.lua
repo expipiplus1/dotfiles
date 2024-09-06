7e2c24762573805d3ffc86d7417ae0ae4a7c25a8
Ellie Hermaszewska <git@monoid.al>
2024-09-06 15:10:02 +0800

    bigger buffers

--- home/.config/astrovim/lua/plugins/astrocore.lua
+++ home/.config/astrovim/lua/plugins/astrocore.lua
@@ -8,7 +8,7 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      large_buf = { size = 4096 * 256, lines = 30000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = false,                                -- enable autopairs at start
       cmp = true,                                       -- enable completion at start
       diagnostics_mode = 3,                             -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
