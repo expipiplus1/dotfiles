2552bb4786eab1af1340ddff6507ea09e80f49fa
Ellie Hermaszewska <git@monoid.al>
2024-09-06 09:22:19 +0800

    neater context

--- home/.config/astrovim/lua/plugins/nvim-treesitter-context.lua
+++ home/.config/astrovim/lua/plugins/nvim-treesitter-context.lua
@@ -3,11 +3,6 @@ return {
   "nvim-treesitter-context",
   opts = {
     max_lines = 4,
+    multiline_threshold = 1,
   },
-  -- dependencies = {
-  --   "AstroNvim/astroui",
-  --   opts = function(_, opts)
-  --     opts.highlights.init
-  --   end,
-  -- },
 }
