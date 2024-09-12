0133bcd6b395b54398527330889cbc465d8d9214
Ellie Hermaszewska <git@monoid.al>
2024-09-12 09:56:17 +0800

    commentstring

--- home/.config/astrovim/lua/plugins/nvim-ts-context-commentstring.lua
+++ home/.config/astrovim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,5 +1,10 @@
 ---@type LazySpec
 return {
   "JoosepAlviste/nvim-ts-context-commentstring",
-  opts = function(_, opts) opts.languages.hlsl = "// %s" end,
+  enabled = false,
+  opts = function(_, opts)
+    opts.enable = true
+    opts.languages = {}
+    opts.languages.hlsl = "// %s"
+  end,
 }
