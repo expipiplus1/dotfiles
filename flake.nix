d31c7f108368455d751c3f1a95c063a75cbdb33c
Ellie Hermaszewska <git@monoid.al>
2024-09-19 14:59:22 +0800

    bump

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
