e558554558936a06779ecdc473edb35f45e7f06b
Ellie Hermaszewska <git@monoid.al>
2025-06-24 11:35:54 +0800

    better markdown render options

---
+++ home/.config/astronvim/lua/plugins/render-markdown-nvim.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  "render-markdown-nvim",
+  opts = {
+    code = { border = "thin" },
+    latex = { enabled = false },
+    pipe_table = { style = "normal" },
+  },
+}
