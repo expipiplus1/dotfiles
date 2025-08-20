fa44f5e21003a4938b3d0439acb17268d7826c02
Ellie Hermaszewska <git@monoid.al>
2025-08-20 20:52:30 +0800

    macos init

---
+++ homes/aarch64-darwin/ellieh/default.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  # ellie.autostart.enable = true;
+  # ellie.dual-boot.enable = true;
+  # ellie.pc.enable = true;
+  # ellie.sensors.enable = true;
+  ellie.common.enable = true;
+}
