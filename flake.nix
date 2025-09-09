6b02993d4a35b51cfbae526431b9194c37fd5e18
Ellie Hermaszewska <git@monoid.al>
2025-09-09 22:38:03 +0800

    bump

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
       url = "git+https://git.lix.systems/lix-project/nixos-module";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence = {
