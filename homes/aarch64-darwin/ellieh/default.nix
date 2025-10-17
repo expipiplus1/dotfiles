7354ff71174b390be4f56a15af3b37d52cfa2cd0
Ellie Hermaszewska <git@monoid.al>
2025-10-17 10:33:11 +0800

    claude

--- homes/aarch64-darwin/ellieh/default.nix
+++ homes/aarch64-darwin/ellieh/default.nix
@@ -1,7 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   # ellie.autostart.enable = true;
   # ellie.dual-boot.enable = true;
   # ellie.pc.enable = true;
   # ellie.sensors.enable = true;
   ellie.common.enable = true;
+
+  home.packages = with pkgs; [
+    claude-code
+  ];
 }
