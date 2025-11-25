3c50533ccde0f02f046a5f568803b78623a119e8
Ellie Hermaszewska <git@monoid.al>
2025-11-25 14:40:48 +0800

    fiddle highlight

---
+++ home/.config/astronvim/lua/plugins/fiddle-highlight.lua
@@ -0,0 +1,3 @@
+return {
+  dir = vim.fn.stdpath "config" .. "/custom-plugins/fiddle-highlight",
+}
--- home/.config/astronvim/queries/cpp/injections.scm
+++
@@ -1,3 +0,0 @@
-; highlight any line that starts with `%` as Lua
-((ERROR) @lua
-  (#match? @lua "^%s*%+?.*"))
