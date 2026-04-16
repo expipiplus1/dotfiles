8bea655efd2d9c0fb9fb913b49e5ef3c9fa7238a
Ellie Hermaszewska <git@monoid.al>
2026-04-16 09:24:30 +0800

    remove custom memtest86plus 5.31b derivation (superseded by nixpkgs 7.20+)

--- overlays/versions/default.nix
+++ overlays/versions/default.nix
@@ -85,42 +85,6 @@ self: super: {
   atuin = super.atuin.overrideAttrs (old: rec {
     patches = old.patches or [ ] ++ [ ../patches/atuin-popup.patch ];
   });
-  memtest86plus = self.callPackage (
-    {
-      stdenv,
-      fetchurl,
-      lib,
-    }:
-    stdenv.mkDerivation rec {
-      pname = "memtest86+";
-      version = "5.31b";
-
-      src = fetchurl {
-        url = "https://www.memtest.org/download/${version}/memtest86+-${version}.tar.gz";
-        sha256 = "028zrch87ggajlb5xx1c2ab85ggl9qldpibf45735sy0haqzyiki";
-      };
-
-      hardeningDisable = [ "all" ];
-
-      doCheck = stdenv.isi686;
-      checkTarget = "run_self_test";
-
-      installPhase = ''
-        install -Dm0444 -t $out/ memtest.bin
-      '';
-
-      meta = with lib; {
-        homepage = "https://www.memtest.org/";
-        description = "An advanced memory diagnostic tool";
-        license = licenses.gpl2Only;
-        platforms = [
-          "x86_64-linux"
-          "i686-linux"
-        ];
-        maintainers = with maintainers; [ evils ];
-      };
-    }
-  ) { };
 
   # starship = super.starship.override {
   #   rustPlatform = self.rustPlatform // {
